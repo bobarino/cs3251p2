030c9b09e25cea5ccd58d448d12f2f7fbd68c73b
Nick Giammanco <ngiammanco@gmail.com>
2018-03-16 15:58:53 -0400

    Got ringos to 1 POC working. WIP for ringo -> ringo -> POC

--- ringo.py
+++ ringo.py
@@ -5,55 +5,102 @@ from threading import Thread
 import numpy as np
 
 class Server(Thread):
-    def __init__(self, host, port):
-        Thread.__init__(self)
-        self.port = port
-        self.host = host
-        self.bufsize = 1024
-        self.addr = (host, port)
-
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.bind(self.addr)
-
-        self.ringo_vector = []
-
-    def run(self):
-        while True:
-            #print 'Waiting for connection..'
-            data, address = self.socket.recvfrom(BUFFER_SIZE)
-            #print 'Connected To', address
-            if address not in self.ringo_vector:
-                self.ringo_vector.append(address)
-            if not data:
-                continue
-            if data == "PD":
-                response = ""
-                for r in self.ringo_vector:
-                    response += str(r[0]) + "," + str(r[1]) + ";"
-                self.socket.sendto(response, address)
-            else:
-                self.socket.sendto(data, address)
-
-class Client(Thread):
-    def __init__(self, flag, host, port, n):
+    def __init__(self, flag, udp_port, poc_name, poc_port, n):
         Thread.__init__(self)
         self.flag = flag
-        self.port = port
-        self.host = host
+        self.udp_port = udp_port
+        self.poc_name = poc_name
+        self.poc_port = poc_port
         self.n = n
-        self.bufsize = 1024
-        self.addr = (host, port)
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.local_server_addr = (socket.gethostbyname(socket.gethostname()), self.udp_port)
+        self.socket.bind(self.local_server_addr)
+
+        self.ringo_vector = []
+        #if self.poc_name == "0" and self.poc_port == 0:
+            #self.ringo_vector.append(self.local_server_addr)
+            #print self.ringo_vector
 
     def run(self):
         while True:
-            data = raw_input('> ')
+            print 'Waiting for connection..'
+            #Need to do peer discoery is when packet is FALSE
+            data, address = self.socket.recvfrom(BUFFER_SIZE)
+            #print 'Connected To', address
             if not data:
                 continue
-            self.socket.sendto(data, self.addr)
-            response, server = self.socket.recvfrom(BUFFER_SIZE)
-            print response
+            if data.split(";")[0] == "PD":
+                self.peer_discovery_append(address, data.split(";")[1])
+                self.peer_discover_send()
+            #else:
+            #    self.socket.sendto(data, address)
+
+    def peer_discovery_append(self, client_address, server_port):
+        print "PD Started"
+        new_ringo = client_address + (server_port,)
+        print new_ringo
+        if new_ringo not in self.ringo_vector:
+            self.ringo_vector.append(new_ringo)
+        #response = ""
+        #for r in self.ringo_vector:
+        #    response += str(r[0]) + "," + str(r[1]) + ";"
+        #self.socket.sendto(response, address)
+
+    def peer_discover_send(self):
+        response = "PDSENDACK;"
+        for r in self.ringo_vector:
+            response += str(r[0]) + "," + str(r[2]) + ";"
+        for r in self.ringo_vector:
+            r_addr = (r[0], int(r[1]))
+            #print r_addr
+            self.socket.sendto(response, r_addr)
+            data, address = self.socket.recvfrom(BUFFER_SIZE)
+            #print data
+
+
+class Client(Thread):
+    def __init__(self, flag, udp_port, poc_name, poc_port, n):
+        Thread.__init__(self)
+        self.flag = flag
+        self.udp_port = udp_port
+        self.poc_name = poc_name
+        self.poc_port = poc_port
+        self.n = n
+        self.ringo_vector = []
+
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.local_server_addr = (socket.gethostbyname(socket.gethostname()), self.udp_port)
+        self.local_addr = (socket.gethostbyname(socket.gethostname()), self.socket.getsockname()[1])
+        #print self.local_addr
+        #Make so you have to do the gethostbyname function
+        if self.poc_name != "0" and self.poc_port != 0:
+            print "SENDING"
+            self.poc_addr = (self.poc_name, self.poc_port)
+            self.socket.sendto("PD;" + str(self.udp_port), self.poc_addr)
+        else:
+            self.socket.sendto("PD;" + str(self.udp_port), self.local_server_addr)
+
+    def run(self):
+        while True:
+            #self.socket.sendto("PD", self.poc_addr)
+            # data = raw_input('> ')
+            # if not data:
+            #     continue
+            # self.socket.sendto(data, self.poc_addr)
+            data, address = self.socket.recvfrom(BUFFER_SIZE)
+            if data.split(";")[0] == "PDSENDACK":
+                for d in data.split(";")[1:]:
+                    if d not in self.ringo_vector:
+                        self.ringo_vector.append(d)
+                self.ringo_vector.pop()
+                if self.poc_name != "0" and self.poc_port != 0:
+                    self.socket.sendto("PDRECACK", self.poc_addr)
+                else:
+                    self.socket.sendto("PDRECACK", self.local_server_addr)
+            #print data
+            print self.ringo_vector
             # if data == "PD":
             #     response_split = response.split(";")
             #     response_split.pop()
@@ -75,17 +122,18 @@ if __name__ == "__main__":
         sys.exit(1)
 
     FLAG = sys.argv[1]
-    UDP_PORT = 50000
-    if int(sys.argv[2]) > 49152 and int(sys.argv[2]) < 65535:
-        UDP_PORT = int(sys.argv[2])
+    #FIX LATER
+    #UDP_PORT = 50000
+    #if int(sys.argv[2]) > 49152 and int(sys.argv[2]) < 65535:
+    UDP_PORT = int(sys.argv[2])
     BUFFER_SIZE = 1024
     POC_NAME = sys.argv[3]
     POC_PORT = int(sys.argv[4])
     N = sys.argv[5]
 
 
-    server = Server(POC_NAME, UDP_PORT)
-    client = Client(FLAG, POC_NAME, POC_PORT, N)
+    server = Server(FLAG, UDP_PORT, POC_NAME, POC_PORT, N)
+    client = Client(FLAG, UDP_PORT, POC_NAME, POC_PORT, N)
 
     server.start()
     client.start()
